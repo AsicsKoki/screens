48909d4d7a35c33836d3f1e6f472505420de0116
Konstantin <cpt.koki@gmail.com>
2016-06-08 18:25:22 +0200

    initial

---
+++ storage/framework/views/f33d39979c3edc8d97bb91b7544e009d868ed47f.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>screens</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <link href="<?php echo asset('css/main.css'); ?>" media="all" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">images below</div>
+            </div>
+        </div>
+    <script type="text/javascript" src="<?php echo asset('js/main.js'); ?>"></script>
+    </body>
+</html>
